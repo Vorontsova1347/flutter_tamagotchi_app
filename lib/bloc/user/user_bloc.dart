d7c80d8595db350b4398668a11024c85d85d6792
Sveta Vorontsova <34057593+Vorontsova1347@users.noreply.github.com>
2022-03-02 05:01:59 +0300

    Revert "refactor(whole-project): architecture refactoring"
    This reverts commit 02990d66d758ed513d56dc6562b90ef7a1e43669.

---
+++ lib/bloc/user/user_bloc.dart
@@ -0,0 +1,34 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'user_event.dart';
+
+part 'user_state.dart';
+
+class UserBloc extends Bloc<UserEvent, UserState> {
+  UserBloc() : super(UserInitial()) {
+    on<AuthorizationEvent>((event, emit) {
+      // TODO: implement event handler
+      print('Success');
+      if (event.login == 'admin' && event.password == '123456789') {
+        print(event.login);
+        print(event.password);
+        emit(AuthSuccessful());
+      } else {
+        emit(AuthFailed());
+      }
+    });
+
+    on<RegistrationEvent>((event, emit) {
+      // TODO: implement event handler
+      emit(RegistrationSuccessful());
+    });
+
+    on<LogOutEvent>((event, emit) {
+      // TODO: implement event handler
+      emit(LogOutSuccessful());
+    });
+  }
+}
