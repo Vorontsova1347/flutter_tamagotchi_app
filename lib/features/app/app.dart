d7984dc68f98dfc711575d56b5ef3f63c28ca52c
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-02 05:41:09 +0300

    feat(app): added app model and widget model

---
+++ lib/features/app/app.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: avoid_redundant_argument_values
+
+import 'package:flutter/material.dart';
+import 'package:mwwm/mwwm.dart';
+import 'package:surf_injector/surf_injector.dart';
+import 'package:tamagochi_app/assets/themes/app_themes.dart';
+import 'package:tamagochi_app/config/debug_options.dart';
+import 'package:tamagochi_app/features/app/app_widget_model.dart';
+import 'package:tamagochi_app/features/app/di/app_component.dart';
+import 'package:tamagochi_app/features/navigation/app_router.dart';
+
+/// [AppWidgetModel] корневого приложения
+class App extends CoreMwwmWidget<AppWidgetModel> {
+  App({Key? key})
+      : super(
+          key: key,
+          widgetModelBuilder: (_) => AppWidgetModel(),
+        );
+
+  @override
+  WidgetState<App, AppWidgetModel> createWidgetState() => _AppState();
+}
+
+/// [WidgetState] модельки корневого приложения
+class _AppState extends WidgetState<App, AppWidgetModel> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: defaultTheme,
+      navigatorKey: Injector.of<AppComponent>(context).component.navigator,
+      showPerformanceOverlay: DebugOptions.showPerformanceOverlay,
+      debugShowMaterialGrid: DebugOptions.debugShowMaterialGrid,
+      checkerboardRasterCacheImages: DebugOptions.checkerboardRasterCacheImages,
+      checkerboardOffscreenLayers: DebugOptions.checkerboardOffscreenLayers,
+      showSemanticsDebugger: DebugOptions.showSemanticsDebugger,
+      debugShowCheckedModeBanner: DebugOptions.debugShowCheckedModeBanner,
+      initialRoute: AppRouter.splashScreen,
+      onGenerateRoute: (routeSettings) =>
+          AppRouter.routes[routeSettings.name]!(routeSettings.arguments),
+    );
+  }
+}
