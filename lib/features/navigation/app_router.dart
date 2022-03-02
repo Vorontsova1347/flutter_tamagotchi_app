bcfc443ec4d259b63bd48627badf9180d70d8d2d
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-02 04:59:51 +0300

    feat(navigation): added realisation for app_router.dart
    The AppRouter class has been created and prepared for use, examples are included

---
+++ lib/features/navigation/app_router.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class AppRouter {
+  static const String loginScreen = '/login';
+  static const String onboardingScreen = '/onboarding';
+  static const String splashScreen = '/';
+
+  static final Map<String, Route Function(Object?)> routes = {
+    // TODO(svetlana): add login screen route
+    //loginScreen: (_) => LoginScreenRoute(),
+
+    // TODO(svetlana): add onboarding screen route
+    //onboardingScreen: (_) => OnboardingScreenRoute(),
+
+    // TODO(svetlana): add splashScreen screen route
+    //splashScreen: (_) => SplashScreenRoute(),
+  };
+}
