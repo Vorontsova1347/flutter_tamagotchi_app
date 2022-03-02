1d769bb13adc086c450ff1591d38e2894802501e
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-02 05:41:09 +0300

    feat(app): added app model and widget model

---
+++ lib/features/app/app_widget_model.dart
@@ -0,0 +1,6 @@
+import 'package:mwwm/mwwm.dart';
+
+/// [WidgetModel] корневого приложения
+class AppWidgetModel extends WidgetModel {
+  AppWidgetModel() : super(const WidgetModelDependencies());
+}
