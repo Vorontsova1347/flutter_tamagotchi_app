075129adfd97d13024dfec4c1d7c821dba4bb4c5
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-03 02:49:38 +0300

    refactor(app): new docs and create function for wm

--- lib/features/app/di/app_component.dart
+++ lib/features/app/di/app_component.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:surf_injector/surf_injector.dart';
 
-/// [Component] для зависимостей всего приложения
+/// [Component] for app
 class AppComponent extends Component {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final navigator = GlobalKey<NavigatorState>();
