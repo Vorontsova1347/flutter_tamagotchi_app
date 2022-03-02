8e99814c104ac30e2039468e6ee6514b3ca14d16
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-02 05:40:44 +0300

    feat(config): added debug options file

---
+++ lib/config/debug_options.dart
@@ -0,0 +1,11 @@
+/// (ВРЕМЕННЫЙ) Класс настроек для дебага
+class DebugOptions {
+  static const bool showPerformanceOverlay = false;
+  static const bool debugShowMaterialGrid = false;
+  static const bool checkerboardRasterCacheImages = false;
+  static const bool checkerboardOffscreenLayers = false;
+  static const bool showSemanticsDebugger = false;
+  static const bool debugShowCheckedModeBanner = false;
+  static const bool debugUseMocker = false;
+  static const bool devicePreview = true;
+}
