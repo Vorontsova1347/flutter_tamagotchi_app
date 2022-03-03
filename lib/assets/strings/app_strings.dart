691fda1d4af7e732d4b81b8e09cc8725a524b68b
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-03 20:37:45 +0300

    feat(assets): new color, strings and fontStyles

--- lib/assets/strings/app_strings.dart
+++ lib/assets/strings/app_strings.dart
@@ -1,4 +1,6 @@
 class AppStrings {
   static const String splashScreenTitleTopText = 'lego';
   static const String splashScreenTitleBottomText = 'Tamagochi';
+  static const String authScreenButtonText = 'log in';
+  static const String authScreenUnderButtonText = 'sign in';
 }
