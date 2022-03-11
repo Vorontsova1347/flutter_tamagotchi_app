141c376caf8b5dcb05a4abdbf156aa4d423b3763
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-11 10:13:59 +0300

    feat(api-data): added registration_data

--- lib/api/data/registration_data/registration_data.g.dart
+++ lib/api/data/registration_data/registration_data.g.dart
@@ -6,12 +6,11 @@ part of 'registration_data.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-RegistrationData _$RegistrationDataFromJson(Map<String, dynamic> json) {
-  return RegistrationData(
-    name: json['name'] as String,
-    password: json['password'] as String,
-  );
-}
+RegistrationData _$RegistrationDataFromJson(Map<String, dynamic> json) =>
+    RegistrationData(
+      name: json['name'] as String,
+      password: json['password'] as String,
+    );
 
 Map<String, dynamic> _$RegistrationDataToJson(RegistrationData instance) =>
     <String, dynamic>{
