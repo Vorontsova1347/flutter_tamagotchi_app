1b7e59ef31cabe4ee3029f0f009632060b8b3eb1
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-11 10:17:38 +0300

    refactor(config): option files rework

--- lib/config/screen_util_options.dart
+++ lib/config/screen_util_options.dart
@@ -1,4 +1,6 @@
-////// (ВРЕМЕННЫЙ) Класс настроек для дебага
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+/// Temp options to init [ScreenUtil]
 class ScreenUtilOptions {
   static const double defaultDesignWidth = 360.0;
   static const double defaultDesignHeight = 640.0;
