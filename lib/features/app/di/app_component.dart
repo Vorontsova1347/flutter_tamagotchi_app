48c7b781e4334294055934298962bbaf66985688
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-11 10:18:25 +0300

    feat(di-component): added repositoires, interactor, access_data

--- lib/features/app/di/app_component.dart
+++ lib/features/app/di/app_component.dart
@@ -1,9 +1,24 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:surf_injector/surf_injector.dart';
+import 'package:tamagochi_app/api/data/access_data/access_data.dart';
+import 'package:tamagochi_app/api/service/api_client.dart';
+import 'package:tamagochi_app/config/api_urls.dart';
+import 'package:tamagochi_app/config/dio_options.dart';
+import 'package:tamagochi_app/features/common/domain/interactors/user/user_interactor.dart';
+import 'package:tamagochi_app/features/common/domain/repository/user/user_repository.dart';
 
-/// [Component] for app
+/// DI [Component] for app
 class AppComponent extends Component {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final navigator = GlobalKey<NavigatorState>();
+  final accessToken = AccessData();
 
+  late final userRepository = UserRepository(
+    client: _apiClient,
+    accessData: accessToken,
+  );
+  late final userInteractor = UserInteractor(userRepository);
+  late final _dio = Dio(dioOptions);
+  late final _apiClient = ApiClient(_dio, baseUrl: ApiUrls.defautlDioUrl);
 }
