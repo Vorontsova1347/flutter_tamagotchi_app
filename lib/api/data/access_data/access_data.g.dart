a4c7e2c3fc8a79d0230508382c2e9d86006d192a
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-11 10:13:30 +0300

    feat(api-data): added acces_data

---
+++ lib/api/data/access_data/access_data.g.dart
@@ -0,0 +1,18 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'access_data.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+AccessData _$AccessDataFromJson(Map<String, dynamic> json) => AccessData(
+      access_token: json['access_token'] as String? ?? null,
+      id: json['id'] as int? ?? null,
+    );
+
+Map<String, dynamic> _$AccessDataToJson(AccessData instance) =>
+    <String, dynamic>{
+      'access_token': instance.access_token,
+      'id': instance.id,
+    };
