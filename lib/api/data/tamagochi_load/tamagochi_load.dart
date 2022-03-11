cbf319b151362910ad5496068fc2c90521f635c3
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-11 10:14:51 +0300

    feat(api-data): added tamagochi_load

--- lib/api/data/tamagochi_load/tamagochi_load.dart
+++ lib/api/data/tamagochi_load/tamagochi_load.dart
@@ -3,20 +3,25 @@ part 'tamagochi_load.g.dart';
 
 @JsonSerializable()
 class TamagochiLoad {
-  final double? sleep;
-  final double? health;
-  final double? game;
-  final double? food;
+  final double sleep;
+  final double health;
+  final double game;
+  final double food;
 
-  TamagochiLoad(
-    this.sleep,
-    this.health,
-    this.game,
-    this.food,
-  );
+  TamagochiLoad({
+    required this.sleep,
+    required this.health,
+    required this.game,
+    required this.food,
+  });
 
   factory TamagochiLoad.fromJson(Map<String, dynamic> json) =>
       _$TamagochiLoadFromJson(json);
+
+  @override
+  String toString() {
+    return 'TamagochiLoad: {\n sleep: $sleep,\n health: $health,\n game: $game,\n food: $food,\n}';
+  }
 
   Map<String, dynamic> toJson() => _$TamagochiLoadToJson(this);
 }
