f421d1d93edb05823daf87ff9cd38781bc098ddc
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-11 10:14:35 +0300

    feat(api-data): added tamagochi_dto

---
+++ lib/api/data/tamagochi_dto/tamagochi_dto.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:json_annotation/json_annotation.dart';
+part 'tamagochi_dto.g.dart';
+
+@JsonSerializable()
+class TamagochiDto {
+  final double? sleep;
+  final double? health;
+  final double? game;
+  final double? food;
+  final int? user_id;
+  final int? id;
+  final double? general_state;
+
+  TamagochiDto({
+    this.sleep,
+    this.health,
+    this.game,
+    this.food,
+    this.user_id,
+    this.id,
+    this.general_state,
+  });
+
+  factory TamagochiDto.fromJson(Map<String, dynamic> json) =>
+      _$TamagochiDtoFromJson(json);
+
+  @override
+  String toString() {
+    return 'TamagochiDto: {\n sleep: $sleep,\n health: $health,\n game: $game,\n food: $food,\n sleep: $sleep,\n user_id: $user_id,\n id: $id,\n general_state: $general_state,\n}';
+  }
+
+  Map<String, dynamic> toJson() => _$TamagochiDtoToJson(this);
+}
---
+++ lib/api/data/tamagochi_dto/tamagochi_dto.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'tamagochi_dto.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+TamagochiDto _$TamagochiDtoFromJson(Map<String, dynamic> json) => TamagochiDto(
+      sleep: (json['sleep'] as num?)?.toDouble(),
+      health: (json['health'] as num?)?.toDouble(),
+      game: (json['game'] as num?)?.toDouble(),
+      food: (json['food'] as num?)?.toDouble(),
+      user_id: json['user_id'] as int?,
+      id: json['id'] as int?,
+      general_state: (json['general_state'] as num?)?.toDouble(),
+    );
+
+Map<String, dynamic> _$TamagochiDtoToJson(TamagochiDto instance) =>
+    <String, dynamic>{
+      'sleep': instance.sleep,
+      'health': instance.health,
+      'game': instance.game,
+      'food': instance.food,
+      'user_id': instance.user_id,
+      'id': instance.id,
+      'general_state': instance.general_state,
+    };
