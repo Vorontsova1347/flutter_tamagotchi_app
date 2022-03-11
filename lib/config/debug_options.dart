76d5bc9d774032d254078ae2adbd2e259bbf5d97
svetlana <svetlana.voroncova.1999@mail.ru>
2022-03-11 10:17:38 +0300

    refactor(config): option files rework

--- lib/config/debug_options.dart
+++ lib/config/debug_options.dart
@@ -1,5 +1,6 @@
-///TODO(svetlana): update temp debugOptions
-////// (ВРЕМЕННЫЙ) Класс настроек для дебага
+import 'package:flutter/material.dart';
+
+/// Temp options to configure [MaterialApp] parameters
 class DebugOptions {
   static const bool showPerformanceOverlay = false;
   static const bool debugShowMaterialGrid = false;
@@ -7,5 +8,4 @@ class DebugOptions {
   static const bool checkerboardOffscreenLayers = false;
   static const bool showSemanticsDebugger = false;
   static const bool debugShowCheckedModeBanner = false;
-  static const bool debugUseMocker = false;
 }
